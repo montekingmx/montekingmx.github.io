be4e090810dac91cab70c4750ae952810a8139ef
montekingmx <ogcasila@gmail.com>
2026-01-30 22:30:40 -0600

    subiendo web catalog completo

---
+++ Monteking Catalogo WEB/playlist.js
@@ -0,0 +1,498 @@
+var songs = [
+    {
+        "name": "Yyy Sour Play || Beat ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/yyy_sour_play_||_beat_||_116bpm.mp3",
+        "bpm": 116,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "What U Need || Beat || Trap Monteking",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/what_u_need_||_beat_||_trap_monteking_121bpm.mp3",
+        "bpm": 121,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Payaso De Circo || Beat || Memph Trap Monteking ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/payaso_de_circo_||_beat_||_memph_trap_monteking_||_110bpm_112bpm.mp3",
+        "bpm": 110,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Violin Negro || Beat || Trap",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/violin_negro_||_beat_||_trap_107bpm.mp3",
+        "bpm": 107,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "No Traen Nada || Beat || Trap Memphis Monteking ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/no_traen_nada_||_beat_||_trap_memphis_monteking_||_125bpm_106bpm.mp3",
+        "bpm": 125,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Acicalado || Beat || Trap Memph Monteking ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/acicalado_||_beat_||_trap_memph_monteking_||_122bpm.mp3",
+        "bpm": 122,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "327 || Beat || Trap Memphis Fresh ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/327_||_beat_||_trap_memphis_fresh_||_124bpm_123bpm.mp3",
+        "bpm": 124,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Je' Cherche Le Billets Pa Les Pies 2 || Beat || 120 Bpm || Trap Deep Monteking",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/je'_cherche_le_billets_pa_les_pies_2_||_beat_||_120_bpm_||_trap_deep_monteking.mp3",
+        "bpm": 0,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Sun Boss || Beat || Trap Monteking Housto\u0301N ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/sun_boss_||_beat_||_trap_monteking_housto\u0301n_||_135bpm.mp3",
+        "bpm": 135,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Gota De Hielo || Beat || Trap Tumbado",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/gota_de_hielo_||_beat_||_trap_tumbado_113bpm.mp3",
+        "bpm": 113,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Necromancer || Beat || Trap Memphis Monteking ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/necromancer_||_beat_||_trap_memphis_monteking_||_125bpm_121bpm.mp3",
+        "bpm": 125,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Ades || Beat || Trap Monteking",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/ades_||_beat_||_trap_monteking_117bpm.mp3",
+        "bpm": 117,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Avem || Beat ||  || Trap Monteking",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/avem_||_beat_||_60bpm_||_trap_monteking_121bpm.mp3",
+        "bpm": 60,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Suave Y Denso || Slow Beat || Lofi",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/suave_y_denso_||_slow_beat_||_lofi_112bpm.mp3",
+        "bpm": 112,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "In My Bag || Beat || Memph Monteking Detroit Trap",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/in_my_bag_||_beat_||_memph_monteking_detroit_trap_126bpm.mp3",
+        "bpm": 126,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Snake || Drill || Beat",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/snake_||_drill_||_beat__134bpm.mp3",
+        "bpm": 134,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Tomahawk || Beat || Trap Monteking ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/tomahawk_||_beat_||_trap_monteking_||_130bpm_124bpm.mp3",
+        "bpm": 130,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "24K || Beat || Trap ||",
+        "artist": "Monteking",
+        "album": "TRAP-MEMPH",
+        "url": "TRAP-MEMPH/24k_||_beat_||_trap_||_112bpm_111bpm.mp3",
+        "bpm": 112,
+        "cover_art_url": "ASSETS/cover_trap.jpg"
+    },
+    {
+        "name": "Devil || Beat || Boombap",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/devil_||_beat_||_boombap_154bpm.mp3",
+        "bpm": 154,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Sencillo || Beat || Boombap Classic ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/sencillo_||_beat_||_boombap_classic_||_94bpm_114bpm.mp3",
+        "bpm": 94,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Stairway Class || Beat || Ohshit Boombap Classic ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/stairway_class_||_beat_||_ohshit_boombap_classic_||_75bpm_147bpm.mp3",
+        "bpm": 75,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Arrastrando || Beat || Boombap Tumbado ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/arrastrando_||_beat_||_boombap_tumbado_||_140bpm_135bpm.mp3",
+        "bpm": 140,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Guru || Beat || Boombap-Trap",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/guru_||_beat_||_boombap-trap_113bpm.mp3",
+        "bpm": 113,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Slow Reflex || Beat || Boombap Deep Monteking",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/slow_reflex_||_beat_||_boombap_deep_monteking_134bpm.mp3",
+        "bpm": 134,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Bulldozer || Beat || Boombap Duro || 141 Bpm",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/bulldozer_||_beat_||_boombap_duro_||_141_bpm.mp3",
+        "bpm": 0,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Ponganse De Pie || Beat || Boombap Mystik Monteking ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/ponganse_de_pie_||_beat_||_boombap_mystik_monteking_||_135bpm_134bpm.mp3",
+        "bpm": 135,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Falling Harp || Beat || Boombap Dark ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/falling_harp_||_beat_||_boombap_dark_||_115bpm_114bpm.mp3",
+        "bpm": 115,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Bomboycaja || Beat || Boombap Monteking ||  Dminor",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/bomboycaja_||_beat_||_boombap_monteking_||_146bpm_dminor_145bpm.mp3",
+        "bpm": 146,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Muerte Lenta || Beat || Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/muerte_lenta_||_beat_||_boombap_||_122bpm_123bpm.mp3",
+        "bpm": 122,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Starway || Beat || Boombap Funk",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/starway_||_beat_||_boombap_funk__148bpm.mp3",
+        "bpm": 148,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Bien O Mal || Beat || Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/bien_o_mal_||_beat_||_boombap_||_130bpm_131bpm.mp3",
+        "bpm": 130,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Turko Americano || Beat || Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/turko_americano_||_beat_||_boombap_||_125bpm.mp3",
+        "bpm": 125,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Ese Soy || Beat || Boombap Monteking ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/ese_soy_||_beat_||_boombap_monteking_||_119bpm_118bpm.mp3",
+        "bpm": 119,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "G-Funk || Beat || Gfunk Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/g-funk_||_beat_||_gfunk_boombap_||_80bpm_152bpm.mp3",
+        "bpm": 80,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Hace Tiempo || Beat || Cumbia Rap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/hace_tiempo_||_beat_||_cumbia_rap_||_138bpm_139bpm.mp3",
+        "bpm": 138,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "La Barredora || Beat || Boombap Monteking",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/la_barredora_||_beat_||_boombap_monteking_134bpm.mp3",
+        "bpm": 134,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Calabozo || Beat || Boombap Seco ||  C#Minor",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/calabozo_||_beat_||_boombap_seco_||_155bpm_c#minor_151bpm.mp3",
+        "bpm": 155,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Me Chingue La Rodilla || Beat || Boombap Monteking ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/me_chingue_la_rodilla_||_beat_||_boombap_monteking_||_140bpm_139bpm.mp3",
+        "bpm": 140,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Les Confieso || Beat || Boombap Seco",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/les_confieso_||_beat_||_boombap_seco_154bpm.mp3",
+        "bpm": 154,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Digan Lo Que Digan || Beat || Boombap Monteking Tresillo",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/digan_lo_que_digan_||_beat_||_boombap_monteking_tresillo_118bpm.mp3",
+        "bpm": 118,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Crema Y Manteca || Beat || Boombap Mystik Monteking ||  Dminor",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/crema_y_manteca_||_beat_||_boombap_mystik_monteking_||_130bpm_dminor_131bpm.mp3",
+        "bpm": 130,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Vigilando || Beat || Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/vigilando_||_beat_||_boombap_||_120bpm.mp3",
+        "bpm": 120,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Murcie\u0301Lago || Beat || Boombap Seco",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/murcie\u0301lago_||_beat_||_boombap_seco_153bpm.mp3",
+        "bpm": 153,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Ripper || Beat || Boombap Mystik ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/ripper_||_beat_||_boombap_mystik_||_128bpm_127bpm.mp3",
+        "bpm": 128,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Chapter One || Beat || Boombap Dark ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/chapter_one_||_beat_||_boombap_dark_||_60bpm_121bpm.mp3",
+        "bpm": 60,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Donde Piso || Beat || Boombap Mystik Monteking ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/donde_piso_||_beat_||_boombap_mystik_monteking_||_69bpm_154bpm.mp3",
+        "bpm": 69,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Solo Mi\u0301A || Beat || Boombap ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/solo_mi\u0301a_||_beat_||_boombap_||_132bpm.mp3",
+        "bpm": 132,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "High Up || Beat || Boombap || 145 Bpm",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/high_up_||_beat_||_boombap_||_145_bpm.mp3",
+        "bpm": 0,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Horno || Beat || Boombap",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/horno_||_beat_||_boombap_103bpm.mp3",
+        "bpm": 103,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Boiler || Beat || Boombap Mystik Monteking ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/boiler_||_beat_||_boombap_mystik_monteking_||_130bpm.mp3",
+        "bpm": 130,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Nightsleep || Beat || Ohshit Boombap Classic ||",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/nightsleep_||_beat_||_ohshit_boombap_classic_||_120bpm_121bpm.mp3",
+        "bpm": 120,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Slow Burning || Beat || Boombap",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/slow_burning_||_beat_||_boombap__112bpm.mp3",
+        "bpm": 112,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Platinum || Beat || Boombap Seco Monteking",
+        "artist": "Monteking",
+        "album": "BOOMBAP",
+        "url": "BOOMBAP/platinum_||_beat_||_boombap_seco_monteking_138bpm.mp3",
+        "bpm": 138,
+        "cover_art_url": "ASSETS/cover_boombap.jpg"
+    },
+    {
+        "name": "Arriba De Ti || Beat || Lofi Rnb Boombap",
+        "artist": "Monteking",
+        "album": "SUAVE",
+        "url": "SUAVE/arriba_de_ti_||_beat_||_lofi_rnb_boombap_114bpm.mp3",
+        "bpm": 114,
+        "cover_art_url": "ASSETS/cover_suave.jpg"
+    },
+    {
+        "name": "Starway || Beat || Boombap Funk",
+        "artist": "Monteking",
+        "album": "SUAVE",
+        "url": "SUAVE/starway_||_beat_||_boombap_funk_148bpm.mp3",
+        "bpm": 148,
+        "cover_art_url": "ASSETS/cover_suave.jpg"
+    },
+    {
+        "name": "Slow Reflex || Beat || Boombap Deep Monteking",
+        "artist": "Monteking",
+        "album": "SUAVE",
+        "url": "SUAVE/slow_reflex_||_beat_||_boombap_deep_monteking_134bpm.mp3",
+        "bpm": 134,
+        "cover_art_url": "ASSETS/cover_suave.jpg"
+    },
+    {
+        "name": "Ojitos Rojos || Beat || Sushi Body Hot Trap ||",
+        "artist": "Monteking",
+        "album": "SUAVE",
+        "url": "SUAVE/ojitos_rojos_||_beat_||_sushi_body_hot_trap_||_60bpm_119bpm.mp3",
+        "bpm": 60,
+        "cover_art_url": "ASSETS/cover_suave.jpg"
+    },
+    {
+        "name": "Flavors || Beat || Hot Trap ||",
+        "artist": "Monteking",
+        "album": "SUAVE",
+        "url": "SUAVE/flavors_||_beat_||_hot_trap_||_144bpm_127bpm.mp3",
+        "bpm": 144,
+        "cover_art_url": "ASSETS/cover_suave.jpg"
+    },
+    {
+        "name": "Margott || Techno Mk ||",
+        "artist": "Monteking",
+        "album": "TECHNO. MK",
+        "url": "TECHNO. MK/margott_||_techno_mk_||_104bpm_107bpm.mp3",
+        "bpm": 104,
+        "cover_art_url": "ASSETS/cover_techno.jpg"
+    },
+    {
+        "name": "Psyghetto 1  || Techno Mk ||",
+        "artist": "Monteking",
+        "album": "TECHNO. MK",
+        "url": "TECHNO. MK/psyghetto_1__||_techno_mk_||_120bpm_121bpm.mp3",
+        "bpm": 120,
+        "cover_art_url": "ASSETS/cover_techno.jpg"
+    },
+    {
+        "name": "Churpybaby || Techno Mk ||",
+        "artist": "Monteking",
+        "album": "TECHNO. MK",
+        "url": "TECHNO. MK/churpybaby_||_techno_mk_||_113bpm.mp3",
+        "bpm": 113,
+        "cover_art_url": "ASSETS/cover_techno.jpg"
+    },
+    {
+        "name": "Oh Shit || Techno Mk ||",
+        "artist": "Monteking",
+        "album": "TECHNO. MK",
+        "url": "TECHNO. MK/oh_shit_||_techno_mk_||_115bpm_103bpm.mp3",
+        "bpm": 115,
+        "cover_art_url": "ASSETS/cover_techno.jpg"
+    }
+];
